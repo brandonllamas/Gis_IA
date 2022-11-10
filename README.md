4e4708286720832d011e7eb05c8f5154ac162596
Brandon Llamas Larios <brandonllamaslarios@gmail.com>
2022-11-09 20:57:12 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Gis_IA
+ App to count and identify crops in a raster with machine learning
---
+++ modules/method1.py
@@ -0,0 +1,9 @@
+from sklearn.cluster import Birch
+import matplotlib.pyplot as plt
+import fiona
+import rasterio
+from rasterio.plot import show
+from skimage.feature import match_template
+import numpy as np
+from PIL import Image
+
